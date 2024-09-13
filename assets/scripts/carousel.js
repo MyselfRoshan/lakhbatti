f9fa1211ba9e200520c34d0d8e133b54397a31d8
MyselfRoshan <roshanghimire699@protonmail.com>
2024-09-13 23:34:55 +0545

    feat: added upto book our services

---
+++ assets/scripts/carousel.js
@@ -0,0 +1,4 @@
+import Splide from "https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/+esm"
+// console.log("hello")
+var splide = new Splide(".splide")
+splide.mount()
--- assets/scripts/heroCarousel.js
+++ assets/scripts/heroCarousel.js
@@ -1,21 +1,21 @@
-const heroSection = document.getElementById("hero-section");
+const heroSection = document.getElementById("hero-section")
 
 const imageUrls = [
-  "/src/assets/images/c1.jpg",
-  "/src/assets/images/c2.jpg",
-  "/src/assets/images/c3.jpg",
-  "/src/assets/images/c4.jpg",
-];
+    "/src/assets/images/c1.jpg",
+    "/src/assets/images/c2.jpg",
+    "/src/assets/images/c3.jpg",
+    "/src/assets/images/c4.jpg",
+]
 
-let currentImageIndex = 0;
+let currentImageIndex = 0
 
 function changeBackgroundImage() {
-  heroSection.style.backgroundImage = `url(${imageUrls[currentImageIndex]})`;
-  currentImageIndex = (currentImageIndex + 1) % imageUrls.length;
+    heroSection.style.backgroundImage = `url(${imageUrls[currentImageIndex]})`
+    currentImageIndex = (currentImageIndex + 1) % imageUrls.length
 }
 
 // Initial call to set the first background image
-changeBackgroundImage();
+changeBackgroundImage()
 
 // Set an interval to change the background image every 5 seconds (5000 milliseconds)
-setInterval(changeBackgroundImage, 5000);
+setInterval(changeBackgroundImage, 5000)
